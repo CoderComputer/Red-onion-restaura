80c09bddc0fa2f3f25e733ff86ba13be26f9087e
Coder Computer <codercomputer2021@gmail.com>
2020-07-15 02:09:30 +0600

    design complete

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDVKZK6UTzM3z3vjBUHtc50k-PmckELtao",
+    authDomain: "red-onion-test.firebaseapp.com",
+    databaseURL: "https://red-onion-test.firebaseio.com",
+    projectId: "red-onion-test",
+    storageBucket: "red-onion-test.appspot.com",
+    messagingSenderId: "23858914353",
+    appId: "1:23858914353:web:1dbfcbe953a5a7929d2110"
+  };
+
+  export default firebaseConfig;
